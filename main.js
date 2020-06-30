0e3e9ca763c526edfa2d8f3b9cb9f51f20ab2488
Rohini Senthil <rohinivsenthil@gmail.com>
2020-06-30 11:36:14 +0530

    improve copy button

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ const codeBlocks = Array.from(document.getElementsByTagName('pre'));
 
 codeBlocks.forEach(codeBlock => {
   const copyButton = document.createElement("button")
+  const wrapper = document.createElement("div")
 
   copyButton.innerHTML = "Copy"
-  copyButton.style = "height:1rem; width:4rem; font-size:.8rem; background:orange; border:none; margin-bottom:.5rem"
-  codeBlock.parentElement.insertBefore(copyButton, codeBlock)
+  copyButton.style = "height:.5rem; width:4rem; font-size:.7rem; background:#ef8236; border:none; margin-bottom:.5rem"
+  wrapper.appendChild(copyButton)
+  wrapper.style = "width:100%; display:flex; justify-content:flex-end"
+  codeBlock.parentElement.insertBefore(wrapper, codeBlock)
 
   copyButton.onclick = (event) => {
-    var range = document.createRange();
+    const range = document.createRange();
     range.selectNode(codeBlock);
     window.getSelection().removeAllRanges();
     window.getSelection().addRange(range);
