56901122300ece6fc5f93cf40c883245e950850f
Rohini Senthil <rohinivsenthil@gmail.com>
2020-07-01 21:17:59 +0530

    Fixes #1

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 const codeBlocks = Array.from(document.getElementsByTagName('pre'));
-
+const enabledButton = "height:.5rem; width:4rem; font-size:.7rem; border:none; margin-bottom:.5rem";
+const disabledButton = "height:.5rem; width:4rem; font-size:.7rem; border:none; margin-bottom:.5rem; color:gray";
 
 codeBlocks.forEach(codeBlock => {
-  const copyButton = document.createElement("button")
-  const wrapper = document.createElement("div")
+  const copyButton = document.createElement("button");
+  const wrapper = document.createElement("div");
 
-  copyButton.innerHTML = "Copy"
-  copyButton.style = "height:.5rem; width:4rem; font-size:.7rem; border:none; margin-bottom:.5rem"
-  wrapper.appendChild(copyButton)
-  wrapper.style = "width:100%; display:flex; justify-content:flex-end"
-  codeBlock.parentElement.insertBefore(wrapper, codeBlock)
+  copyButton.innerHTML = "Copy";
+  copyButton.style = enabledButton;
+  wrapper.appendChild(copyButton);
+  wrapper.style = "width:100%; display:flex; justify-content:flex-end";
+  codeBlock.parentElement.insertBefore(wrapper, codeBlock);
 
-  copyButton.onclick = (event) => {
-    const range = document.createRange();
-    range.selectNode(codeBlock);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
-    window.getSelection().removeAllRanges()
+  copyButton.onclick = async (event) => {
+    await navigator.clipboard.writeText(codeBlock.innerText)
+    copyButton.innerHTML = "Copied!"
+    copyButton.style = disabledButton;
+    copyButton.disabled = true;
+
+    setTimeout(() => {
+      copyButton.innerHTML = "Copy";
+      copyButton.style = enabledButton;
+      copyButton.disabled = false;
+    }, 2000);
   }
 
 })
