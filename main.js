fbba2df0fd85ac252dc23e3946d328f0dffd6a73
Rohini Senthil <rohinivsenthil@gmail.com>
2020-07-02 16:16:25 +0530

    modify copy button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const codeBlocks = Array.from(document.getElementsByTagName('pre'));
-const enabledButton = "height:.5rem; width:4rem; font-size:.7rem; border:none; margin-bottom:.5rem";
-const disabledButton = "height:.5rem; width:4rem; font-size:.7rem; border:none; margin-bottom:.5rem; color:gray";
+const enabledButton = "font-size:.7rem; border:none; position:absolute; top:0; right:0; background-color: rgba(0, 0, 0, 0); color:gray";
+const disabledButton = "font-size:.7rem; border:none; position:absolute; top:0; right:0; background-color: rgba(0, 0, 0, 0); color:gray";
 
 codeBlocks.forEach(codeBlock => {
   const copyButton = document.createElement("button");
@@ -8,9 +8,14 @@ codeBlocks.forEach(codeBlock => {
 
   copyButton.innerHTML = "Copy";
   copyButton.style = enabledButton;
+  wrapper.appendChild(codeBlock.cloneNode(true))
   wrapper.appendChild(copyButton);
-  wrapper.style = "width:100%; display:flex; justify-content:flex-end";
+  wrapper.style = "position: relative;"
+  console.log(wrapper);
   codeBlock.parentElement.insertBefore(wrapper, codeBlock);
+  codeBlock.remove()
+  console.log('hello');
+
 
   copyButton.onclick = async (event) => {
     await navigator.clipboard.writeText(codeBlock.innerText)
